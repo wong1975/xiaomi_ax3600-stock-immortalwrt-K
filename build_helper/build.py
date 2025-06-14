a66a17f7891f05f9b847a2e58abe899d0fd113c5
wong1975 <43314928+wong1975@users.noreply.github.com>
2025-06-15 03:31:59 +0800

    Update build.py

--- build_helper/build.py
+++ build_helper/build.py
@@ -301,28 +301,16 @@ def build_image_builder(cfg: dict) -> None:
     else:
         logger.warning(f"ipq807x 目录不存在: {ipq807x_path}")
     
-    # 匹配 ImageBuilder 文件
-    bl_path_pattern = os.path.join(openwrt.path, "bin", "targets", target, subtarget, f"*-imagebuilder-*-{target}-{subtarget}.Linux-x86_64.tar.*")
-    files = glob.glob(bl_path_pattern)
 
-    if not files:
-        folder_path = os.path.join(openwrt.path, "bin", "targets", target, subtarget)
-        folder_files = os.listdir(folder_path)  # 获取文件夹下的所有文件
-        logger.debug(f"未找到匹配的 ImageBuilder 文件，当前目录包含：\n{folder_files}")  # 输出文件列表
-        #raise FileNotFoundError("没有找到匹配的 ImageBuilder 文件，请检查路径或文件是否存在")
 
-    else:
-        # 选择第一个匹配的文件
-        bl_path = files[0]
-        ext = bl_path.split(".")[-1]  # 自动获取扩展名
-
-        # 移动文件
-        dest_path = os.path.join(paths.uploads, f"openwrt-imagebuilder.tar.{ext}")
-        shutil.move(bl_path, dest_path)
-
-        # 上传文件
-        uploader.add(f"Image_Builder-{cfg['name']}", dest_path, retention_days=1, compression_level=0)
-
+    bl_path = os.path.join(openwrt.path, "bin", "targets", target, subtarget, f"immortalwrt-imagebuilder-{target}-{subtarget}.Linux-x86_64.tar.zst")
+    ext = "zst"
+    if not os.path.exists(bl_path):
+        bl_path = os.path.join(openwrt.path, "bin", "targets", target, subtarget, f"immortalwrt-imagebuilder-{target}-{subtarget}.Linux-x86_64.tar.xz")
+        ext = "xz"
+    shutil.move(bl_path, os.path.join(paths.uploads, f"openwrt-imagebuilder.tar.{ext}"))
+    bl_path = os.path.join(paths.uploads, f"openwrt-imagebuilder.tar.{ext}")
+    uploader.add(f"Image_Builder-{cfg['name']}", bl_path, retention_days=1, compression_level=0)
 
     # 清理缓存
     logger.info("删除旧缓存...")
