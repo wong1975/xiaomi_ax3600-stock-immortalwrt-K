58ced68cea386f4f733940d36baa98dd27b3d62c
wong1975 <43314928+wong1975@users.noreply.github.com>
2025-06-14 15:48:40 +0800

    Update prepare.py

--- build_helper/prepare.py
+++ build_helper/prepare.py
@@ -145,12 +145,20 @@ def prepare(configs: dict[str, dict[str, Any]]) -> None:
                     elif not os.path.isdir(zh_hans) or not os.path.islink(zh_hans):
                         logger.info("%s 中不存在汉化文件，这可能是该luci插件原生为中文或不支持中文", po_path)
 
+    # 开始克隆
+    logger.info(f"开始克隆仓库源码...: {openwrt_repo}")
+    #openwrt_paths = os.path.join(paths.workdir, "openwrts")
+    #cfg_names = list(configs.keys())
+    #pygit2.clone_repository("https://github.com/openwrt/openwrt", os.path.join(openwrt_paths, cfg_names[0]))
+    #pygit2.clone_repository("https://github.com/immortalwrt/immortalwrt", os.path.join(openwrt_paths, cfg_names[0]))
+    # 获取仓库地址
+    openwrt_repo = os.getenv("OPENWRT_REPO", "https://github.com/openwrt/openwrt")  # 默认 OpenWrt
 
-    logger.info("开始克隆openwrt源码...")
+    # 设置克隆路径
     openwrt_paths = os.path.join(paths.workdir, "openwrts")
     cfg_names = list(configs.keys())
-    #pygit2.clone_repository("https://github.com/openwrt/openwrt", os.path.join(openwrt_paths, cfg_names[0]))
-    pygit2.clone_repository("https://github.com/immortalwrt/immortalwrt", os.path.join(openwrt_paths, cfg_names[0]))
+
+    pygit2.clone_repository(openwrt_repo, os.path.join(openwrt_paths, cfg_names[0]))
 
     # 复制源码
     if len(cfg_names) > 1:
